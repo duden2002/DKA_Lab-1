99857ed170d2bcb1258cd1b35c44ef4d5a6ba3f0
duden2002 <70965647+duden2002@users.noreply.github.com>
2022-09-21 09:02:31 +0600

    Add files via upload

---
+++ MyFunction.py
@@ -0,0 +1,18 @@
+import math
+
+
+def func():
+    a = float(input("Введите а: "))
+    b = float(input("Введите b: "))
+    x = float(input("Введите x: "))
+    try:
+        if (x < 8):
+            y = (6 * (math.pow(a, 2) + x + math.pow(b, 2))) / (a * b * x)
+        else:
+            y = (4 * (math.pow(a, 2) - x + math.pow(b, 2)))
+        print("Ответ: " + format (y, "#.2f"))
+    except:
+        print("Нет решения!")
+
+
+func()
